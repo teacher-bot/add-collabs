27f0f3ecba254c59bbdb59eec25650655525e053
Jason Etcovitch <jasonetco@github.com>
2017-07-26 13:49:48 -0700

    🔧 Pass config as arg, document, cleanup

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-global.__base = __dirname + '/';
-const configs = require('configurable');
-
-// A plugin is a Node module that exports a function which takes a `robot` argument
-module.exports = robot => {
-  robot.on('issues', async context => {
-
+/**
+ * @typedef {Object} Config
+ * @prop {string} existingCollaboratorMessage
+ * @prop {string} newCollaboratorMessage
+ *
+ * Anytime a user opens an issue, add them as a collaborator to the repository.
+ * @param {Object} robot
+ * @param {Config} config
+ */
+module.exports = (robot, config) => {
+  robot.on('issues.opened', async context => {
     const {issue, action} = context.payload;
     const issueOwner = issue.user.login;
     const repo = context.repo({username: issueOwner});
+    const isCollab = await context.github.repos.checkCollaborator(repo)
+      .catch(() => {
+        context.github.repos.addCollaborator(repo);
+      });
+    const params = {
+      number: issue.number,
+      body: isCollab ? config.existingCollaboratorMessage : config.newCollaboratorMessage
+    };
 
-    const config = await configs(context, './lib/defaults');
-
-
-    if (action === 'opened') {
-      const isCollab = await context.github.repos.checkCollaborator(repo)
-        .catch(() => {
-          context.github.repos.addCollaborator(repo);
-        });
-      const params = {
-        number: issue.number,
-        body: isCollab ? config.addCollaborators.existingCollaboratorMessage : config.addCollaborators.newCollaboratorMessage
-      };
-      /// When testing, uncomment the line below and comment the return line -- this avoids the bot getting marked as spam.
-      // robot.log("This is the params", params);
-      return context.github.issues.createComment(context.repo(params));
-    }
+    /// When testing, uncomment the line below and comment the return line -- this avoids the bot getting marked as spam.
+    // robot.log("This is the params", params);
+    return context.github.issues.createComment(context.repo(params));
   });
-
 };
