730f6dca2f6afc1283ec505f6fd5e4d917f3b23f
Hector Alfaro <hectorsector@github.com>
2017-07-27 17:04:20 -0400

    allow defaults to be passed in as a param

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-// A plugin is a Node module that exports a function which takes a `robot` argument
-module.exports = robot => {
-  robot.on('issues', async context => {
+/**
+  * @typedef {Object} Config
+  * @prop {string} existingCollaboratorMessage
+  * @prop {string} newCollaboratorMessage
+  *
+  * Anytime a user opens an issue, add them as a collaborator to the repository.
+  * @param {Object} robot
+  * @param {Config} [defaults]
+  */
+
+module.exports = (robot, defaults = {}) => {
     let config;
-    const defaults = Object.assign({}, {
+
+    defaults = Object.assign({}, {
       addCollaborators: {
         newCollaboratorMessage: 'Hi! I\'m the friendly :robot: of this repo.\n\nWe\'re happy you\'re here :wave:. Everyone is welcome here, so I\'m making you a collaborator. This will give you access to commit on this repo.',
         existingCollaboratorMessage: 'Hi! I\'m the friendly :robot: of this repo.\n\nI can see that you\'re already a collaborator. Any other issues are above my paygrade at the moment, so we\'ll have to wait for a pesky hu-man. Not to worry though, they\'ll drop by within 24 hours to answer your questions!'
       }
-    } || {});
+    }, defaults || {});
+
+    robot.on('issues', async context => {
 
     const {issue, action} = context.payload;
     const issueOwner = issue.user.login;
