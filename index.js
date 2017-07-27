3152569142db1b5a4f9b9926b91989077e2c2432
Hector Alfaro <hectorsector@github.com>
2017-07-27 16:19:53 -0400

    remove unnecessary deps and fix per linter

--- index.js
+++ index.js
@@ -2,26 +2,22 @@
 module.exports = robot => {
   robot.on('issues', async context => {
     let config;
-    const defaults = Object.assign( {}, {
+    const defaults = Object.assign({}, {
       addCollaborators: {
         newCollaboratorMessage: 'Hi! I\'m the friendly :robot: of this repo.\n\nWe\'re happy you\'re here :wave:. Everyone is welcome here, so I\'m making you a collaborator. This will give you access to commit on this repo.',
         existingCollaboratorMessage: 'Hi! I\'m the friendly :robot: of this repo.\n\nI can see that you\'re already a collaborator. Any other issues are above my paygrade at the moment, so we\'ll have to wait for a pesky hu-man. Not to worry though, they\'ll drop by within 24 hours to answer your questions!'
       }
-    } || {} );
+    } || {});
 
     const {issue, action} = context.payload;
     const issueOwner = issue.user.login;
     const repo = context.repo({username: issueOwner});
 
-    let config;
-
     try {
       config = await context.config('teacherbot.yml');
+    } catch (err) {
+      config = defaults;
     }
-      catch(err) {
-        config = defaults;
-      }
-
 
     if (action === 'opened') {
       const isCollab = await context.github.repos.checkCollaborator(repo)
@@ -32,10 +28,9 @@ module.exports = robot => {
         number: issue.number,
         body: isCollab ? config.addCollaborators.existingCollaboratorMessage : config.addCollaborators.newCollaboratorMessage
       };
-      /// When testing, uncomment the line below and comment the return line -- this avoids the bot getting marked as spam.
+      // / When testing, uncomment the line below and comment the return line -- this avoids the bot getting marked as spam.
       // robot.log("This is the params", params);
       return context.github.issues.createComment(context.repo(params));
     }
   });
-
 };
